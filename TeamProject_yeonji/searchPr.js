900541c95e7115e8743e277633be2a20cfdcf809
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2022-08-12 22:08:08 +0900

    뭔지 모르겠지만 아무튼 올림

--- TeamProject_yeonji/searchPr.js
+++ TeamProject_yeonji/searchPr.js
@@ -1,6 +1,3 @@
-
-
-
 //교수 찾기
 function search() {
     var pcheck = document.getElementById("pro").getElementsByTagName("p").length; //p 태그 길이 저장 
@@ -22,7 +19,7 @@ function search() {
         for(var r=0; r<rows.length; r++ ){
             var cells = rows[r].getElementsByTagName("td");
 
-            var cell_1 = cells[1].firstChild.data;		// 이름
+            var cell_1 = cells[1].firstChild.data;      // 이름
 
             if(cell_1==searchPr) { //일치하는 교수 이름이 있으면 출력해주기
                 
@@ -32,26 +29,11 @@ function search() {
                 $("#major").append("<p class='major1'>컴퓨터공학부</p>");
                 $("#bt").append("<p class='bt1'><button id='bt' type='button' onclick=\"location.href=\'possiblePr.html\'\">click</button></p>");
 
-
-                // var prInfo = {
-                //     "pName":cell_1,
-                //     "pMaj":cells[2].firstChild.data,
-                //     "pOff":cells[3].firstChild.data,
-                //     "pMail":cells[4].firstChild.data,
-                //     "pPossible":cells[5].firstChild.data
-                // };
-
-                // localStorage.setItem("pr", JSON.stringify(prInfo));
-
                 localStorage.setItem('pName',cell_1);
                 localStorage.setItem('pMaj',cells[2].firstChild.data);
                 localStorage.setItem('pOff',cells[3].firstChild.data);
                 localStorage.setItem('pMail',cells[4].firstChild.data);
                 localStorage.setItem('pPossible',cells[5].firstChild.data);
-                // pMaj = cells[2].firstChild.data;
-                // pOff = cells[3].firstChild.data;
-                // pMail = cells[4].firstChild.data;
-                // pPossible = cells[5].firstChild.data;
             }
         }
         if(check==0) //check가 0이면 존재하지 않은 이름 출력
@@ -65,90 +47,48 @@ function search() {
     });
 }
 
-
-
 // 다음 페이지로 넘어가면 addRow() 함수 호출됨
 // 선택한 교수님의 정보와 상담 가능 시간을 자세히 보여줌.
-window.onload=function addRow(){
-    //table element 찾기
-    const table = document.getElementById('proTable');
-    //새 행(row) 추가
-    const newRow = table.insertRow();
-    //새 행(row)에 cell 추가
-    newRow.insertCell(0).innerText=localStorage.getItem('pName');
-    newRow.insertCell(1).innerText=localStorage.getItem('pMaj');
-    newRow.insertCell(2).innerText=localStorage.getItem('pOff');
-    newRow.insertCell(3).innerText=localStorage.getItem('pMail');
-    sessionStorage.setItem('proemail',localStorage.getItem('pMail'));
-    //table element 찾기
-    const table2 = document.getElementById('ConTimeTable');
+// window.onload=function addRow(){
+//     //table element 찾기
+//     const table = document.getElementById('proTable');
+//     //새 행(row) 추가
+//     const newRow = table.insertRow();
+//     //새 행(row)에 cell 추가
+//     newRow.insertCell(0).innerText=localStorage.getItem('pName');
+//     newRow.insertCell(1).innerText=localStorage.getItem('pMaj');
+//     newRow.insertCell(2).innerText=localStorage.getItem('pOff');
+//     newRow.insertCell(3).innerText=localStorage.getItem('pMail');
+//     sessionStorage.setItem('proemail',localStorage.getItem('pMail'));
+//     sessionStorage.setItem('proname',localStorage.getItem('pName'));
 
-    //새 행(row)에 cell 추가    
-    let spl = localStorage.getItem('pPossible').split(' ')
-    num=0;
-    for(i=0; i<spl.length; i++)
-    {
-        if(i%2==1)
-        {
-            let spl2 = spl[i].split(',');
+//     //table element 찾기
+//     const table2 = document.getElementById('ConTimeTable');
+
+//     //새 행(row)에 cell 추가    
+//     const spl = localStorage.getItem('pPossible').split(' ');
+//     //const spl = localStorage.getItem('pPossible') || '';
+//     console.log(spl);   
+//     for(i=0; i<spl.length; i++)
+//     {
+//         if(i%2==1)
+//         {
+//             const spl2 = spl[i].split(',');
             
-            for(j=0; j<spl2.length; j++)
-            {
-                for(k=0; k<2; k++)
-                {
-                    //새 행(row) 추가
-                    const newRow2 = table2.insertRow();
-                    newRow2.insertCell(0).innerText=possConsult(spl[i-1]);
-                    newRow2.insertCell(1).innerText=spl2[j]+(String.fromCharCode(65+k));
-                    localStorage.setItem('date',possConsult(spl[i-1]));
-                    localStorage.setItem('time',spl2[j]+(String.fromCharCode(65+k)));
-                    
-                    newRow2.insertCell(2).innerHTML="<input class='btCheck' type='button' value='check'></button>";
-                   
-                }
-            }
-        } 
-    }
-}
-
-// $(document).ready(function() {
-//     $(".btCheck").on('click',function() { 
-//         //console.log("zmff");
-                
-//         var str = ""
-//         var tdArr = new Array();   // 배열 선언
-//         var checkBtn = $(this);
-        
-//         // checkBtn.parent() : checkBtn의 부모는 <td>이다.
-//         // checkBtn.parent().parent() : <td>의 부모이므로 <tr>이다.
-//         var tr = checkBtn.parent().parent();
-//         var td = tr.children();
-        
-//         console.log("클릭한 Row의 모든 데이터 : "+tr.text());
-        
-//         var cDate = td.eq(0).text();
-//         var cTime = td.eq(1).text();
-        
-        
-//         // 반복문을 이용해서 배열에 값을 담아 사용할 수 도 있다.
-//         td.each(function(i){   
-//             tdArr.push(td.eq(i).text());
-//         });
-        
-//         console.log("배열에 담긴 값 : "+tdArr);
-        
-//         str +=   " * 클릭된 Row의 td값 = No. : <font color='red'>" + cDate + "</font>" +
-//                 ", 아이디 : <font color='red'>" + cTime + "</font>";   
-//         // console.log(str);
-//         // console.log(cDate);
-//         // console.log(cTime);
-//         sessionStorage.setItem('date',cDate);
-//         sessionStorage.setItem('time',cTime);
-        
-    
-//     });
-    
-// });
+//             for(j=0; j<spl2.length; j++)
+//             {
+//                 for(k=0; k<2; k++)
+//                 {
+//                     //새 행(row) 추가
+//                     const newRow2 = table2.insertRow();
+//                     newRow2.insertCell(0).innerText=possConsult(spl[i-1]);
+//                     newRow2.insertCell(1).innerText=spl2[j]+(String.fromCharCode(65+k));
+//                     newRow2.insertCell(2).innerHTML="<input class='btCheck' type='button' value='check'></button>";
+//                 }
+//             }
+//         } 
+//     }
+// }
 
 //요일 구하는 function
 function returnWeek(uDay) {
@@ -159,8 +99,6 @@ function returnWeek(uDay) {
     else if(uDay=='목') return 4;
     else if(uDay=='금') return 5;
     else if(uDay=='토') return 6;
-    // var week = ['일', '월', '화', '수', '목', '금', '토'];
-    // var dayOfWeek = week[uDay];   
 }
 
 //상담 가능 시간 날짜로 바꾸는 function
@@ -182,4 +120,3 @@ function possConsult(uDay) {
 
     return dbDate;
 }
-
