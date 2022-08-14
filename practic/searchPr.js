cbf6d5a159d810a9a006ab5d263d19b0ac388f14
JBH-0 <75972885+JBH-0@users.noreply.github.com>
2022-08-14 17:09:48 +0900

    메일 수정 완료

--- practic/searchPr.js
+++ practic/searchPr.js
@@ -1,30 +1,30 @@
 //교수 찾기
 function search() {
-    var pcheck = document.getElementById("pro").getElementsByTagName("p").length; //p 태그 길이 저장 
+    var pcheck = document.getElementById("pro").getElementsByTagName("p").length; //p 태그 길이 저장
 
-    if(pcheck>1){ //p태크 길이가 2개 이상이면 요소 삭제하기 
+    if(pcheck>1){ //p태크 길이가 2개 이상이면 요소 삭제하기
         document.getElementById("pro").removeChild(document.getElementById("pro").childNodes[pcheck+1]);
         document.getElementById("major").removeChild(document.getElementById("major").childNodes[pcheck+1]);
         document.getElementById("bt").removeChild(document.getElementById("bt").childNodes[pcheck+1]);
     }
 
     const searchPr = document.getElementById('formGroupExampleInput').value; //사용자가 입력한 찾는 교수님 이름 searchPr변수에 저장
-    
+   
     $(document).ready(function() {
         var rows = document.getElementById("dataTbl").getElementsByTagName("tr"); //tr요소 찾기
 
         var check=0; //일치하는 이름 있는지 체크
-        
+       
         // tr만큼 루프돌면서 컬럼값 접근
         for(var r=0; r<rows.length; r++ ){
             var cells = rows[r].getElementsByTagName("td");
 
-            var cell_1 = cells[1].firstChild.data;		// 이름
+            var cell_1 = cells[1].firstChild.data;      // 이름
 
             if(cell_1==searchPr) { //일치하는 교수 이름이 있으면 출력해주기
-                
+               
                 check++; //일치하는 이름 있으면 1증가
-                
+               
                 $("#pro").append("<p class='pro1'>"+searchPr+"</p>");
                 $("#major").append("<p class='major1'>컴퓨터공학부</p>");
                 $("#bt").append("<p class='bt1'><button id='bt' type='button' onclick=\"location.href=\'possiblePr.html\'\">click</button></p>");
@@ -43,49 +43,8 @@ function search() {
             $("#bt").append("<p class='bt1'>X</p>");
             alert("존재하지 않습니다.")
         }
-        
+       
     });
-}
-
-//다음 페이지로 넘어가면 addRow() 함수 호출됨
-//선택한 교수님의 정보와 상담 가능 시간을 자세히 보여줌.
-window.onload=function addRow(){
-    //table element 찾기
-    const table = document.getElementById('proTable');
-    //새 행(row) 추가
-    const newRow = table.insertRow();
-    //새 행(row)에 cell 추가
-    newRow.insertCell(0).innerText=localStorage.getItem('pName');
-    newRow.insertCell(1).innerText=localStorage.getItem('pMaj');
-    newRow.insertCell(2).innerText=localStorage.getItem('pOff');
-    newRow.insertCell(3).innerText=localStorage.getItem('pMail');
-    sessionStorage.setItem('proemail',localStorage.getItem('pMail'));
-
-    //table element 찾기
-    const table2 = document.getElementById('ConTimeTable');
-
-    //새 행(row)에 cell 추가    
-    let spl = localStorage.getItem('pPossible').split(' ')
-
-    for(i=0; i<spl.length; i++)
-    {
-        if(i%2==1)
-        {
-            let spl2 = spl[i].split(',');
-            
-            for(j=0; j<spl2.length; j++)
-            {
-                for(k=0; k<2; k++)
-                {
-                    //새 행(row) 추가
-                    const newRow2 = table2.insertRow();
-                    newRow2.insertCell(0).innerText=possConsult(spl[i-1]);
-                    newRow2.insertCell(1).innerText=spl2[j]+(String.fromCharCode(65+k));
-                    newRow2.insertCell(2).innerHTML="<input class='btCheck' type='button' value='check'></button>";
-                }
-            }
-        } 
-    }
 }
 
 //요일 구하는 function
@@ -103,7 +62,7 @@ function returnWeek(uDay) {
 function possConsult(uDay) {
     let cac = returnWeek(uDay); //요일을 숫자로 바꿔주는 함수를 호출해 cac 저장
 
-    let today = new Date();   
+    let today = new Date();  
 
     //상담 가능 기간 동안에서 요일 구하기
     let day = today.getDay();  // 요일(0~6) 일요일0, 월요일1, 토요일6
@@ -113,7 +72,7 @@ function possConsult(uDay) {
     let month = today.getMonth() + 1;  // 월(0~11), 1월은 0
     //기간을 현재 날짜에서 일주일 뒤이기 때문에, 7을 더한다.
     let date = today.getDate()+7+aa;  // 날짜(0~31)
-    
+   
     let dbDate = year + '-' + month + '-' + date;
 
     return dbDate;
